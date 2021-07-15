6c12c9a57d876cd8de28bfc777d2b228cc21f534
MuMuKim <67031535+MuMuKim@users.noreply.github.com>
2021-07-15 13:20:24 +0900

    210715 Main Scene UI 화면 구성

---
+++ Assets/Scripts/PlayerSettngs.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum EControlType
+{
+    Mouse,
+    KeyboardMouse
+}
+public class PlayerSettngs
+{
+    public static EControlType controlType;
+}
---
+++ Assets/UI/Scripts/MainMenuUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public void OnClickonlineButton()
+    {
+        Debug.Log("Click Online");
+    }
+    public void OnClickQuitButton()
+    {
+        //에디터에서 실행된 상태라면 플레이를 중단
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        //빌드된 상태라면 어플리케이션을 종료
+#else
+        Application.Quit();
+#endif
+    }
+}
